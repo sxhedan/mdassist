89417f5d57261e3b85e9c80ff2c64a1e6e43e946
DS <sxhedan@gmail.com>
2021-03-23 22:50:28 -0400

    Set up electron for mdassist.

---
+++ public/electron.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    const startURL = isDev ? 'http://localhost:3000' : url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    mainWindow.loadURL(startURL);
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
